a362003adc4edb69a9c16a763d1467f239471786
va <va@us.ibm.com>
2025-02-27 01:28:04 -0500

    fix: update examples
    Signed-off-by: va <va@us.ibm.com>

--- python/examples/workflows/web_agent.py
+++ python/examples/workflows/web_agent.py
@@ -3,11 +3,12 @@ import sys
 import traceback
 
 from langchain_community.utilities import SearxSearchWrapper
-from pydantic import BaseModel, Field, ValidationError
+from pydantic import BaseModel, Field
 
 from beeai_framework.adapters.ollama.backend.chat import OllamaChatModel
 from beeai_framework.backend.chat import ChatModelOutput, ChatModelStructureOutput
 from beeai_framework.backend.message import UserMessage
+from beeai_framework.errors import FrameworkError
 from beeai_framework.template import PromptTemplate, PromptTemplateInput
 from beeai_framework.workflows.workflow import Workflow, WorkflowError
 
@@ -91,9 +92,7 @@ async def main() -> None:
         print("Input: ", result.state.input)
         print("Agent: ", result.state.output)
 
-    except WorkflowError:
-        traceback.print_exc()
-    except ValidationError:
+    except (WorkflowError, FrameworkError):
         traceback.print_exc()
 
 
