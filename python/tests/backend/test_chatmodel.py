2acf73997fb90d24f64c6e58fe6272cd05b7a941
Nigel Jones <jonesn@uk.ibm.com>
2025-03-03 19:32:08 +0000

    feat(adapters): Groq - correct model for test
    Signed-off-by: Nigel Jones <jonesn@uk.ibm.com>

--- python/tests/backend/test_chatmodel.py
+++ python/tests/backend/test_chatmodel.py
@@ -169,5 +169,5 @@ def test_chat_model_from(monkeypatch: pytest.MonkeyPatch) -> None:
     openai_chat_model = ChatModel.from_name("openai:gpt-4o")
     assert isinstance(openai_chat_model, OpenAIChatModel)
 
-    groq_chat_model = ChatModel.from_name("groq:llama-3.1-8b-instant")
+    groq_chat_model = ChatModel.from_name("groq:gemma2-9b-it")
     assert isinstance(groq_chat_model, GroqChatModel)
