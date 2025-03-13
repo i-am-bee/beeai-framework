726330b40018488f86d202a67d4873b685250f5b
Alex Bozarth <ajbozart@us.ibm.com>
2025-03-13 00:10:29 -0500

    refactor: return typed objects from internal emits
    BREAKING CHANGE: internal emit data types updated
    
    Signed-off-by: Alex Bozarth <ajbozart@us.ibm.com>

--- python/beeai_framework/workflows/events.py
+++ python/beeai_framework/workflows/events.py
@@ -12,13 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Generic
+from typing import Generic, Literal
 
 from pydantic import BaseModel, InstanceOf
 from typing_extensions import TypeVar
 
 from beeai_framework.errors import FrameworkError
-from beeai_framework.workflows.types import WorkflowRun
+from beeai_framework.workflows.types import WorkflowRun, WorkflowStepDefinition
 
 T = TypeVar("T", bound=BaseModel)
 K = TypeVar("K", default=str)
@@ -26,17 +26,17 @@ K = TypeVar("K", default=str)
 
 class WorkflowStartEvent(BaseModel, Generic[T, K]):
     run: WorkflowRun[T, K]
-    step: K
+    step: WorkflowStepDefinition[T, K]
 
 
 class WorkflowSuccessEvent(BaseModel, Generic[T, K]):
     run: WorkflowRun[T, K]
     state: T
-    step: K
-    next: K
+    step: WorkflowStepDefinition[T, K]
+    next: K | Literal["__end__"]
 
 
 class WorkflowErrorEvent(BaseModel, Generic[T, K]):
     run: WorkflowRun[T, K]
-    step: K
+    step: K | Literal["__end__"]
     error: InstanceOf[FrameworkError]
