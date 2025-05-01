5f0806ea475672ea6779155bffe3963737bcb572
Lukáš Janeček <xjacka@gmail.com>
2025-05-01 14:02:32 +0200

    fixup! fixup! feat(adapters): add support for ACP servers
    Signed-off-by: Lukáš Janeček <xjacka@gmail.com>

--- python/examples/serve/acp.py
+++ python/examples/serve/acp.py
@@ -1,5 +1,6 @@
-from beeai_framework.adapters.acp.server import AcpServer
+from beeai_framework.adapters.acp.server import AcpServer, AcpServerConfig
 from beeai_framework.agents.tool_calling.agent import ToolCallingAgent
+from beeai_framework.agents.types import AgentMeta
 from beeai_framework.backend import ChatModel
 from beeai_framework.memory import UnconstrainedMemory
 from beeai_framework.tools.search.duckduckgo import DuckDuckGoSearchTool
@@ -8,9 +9,16 @@ from beeai_framework.tools.weather import OpenMeteoTool
 
 def main() -> None:
     llm = ChatModel.from_name("ollama:granite3.1-dense:8b")
-    agent = ToolCallingAgent(llm=llm, tools=[DuckDuckGoSearchTool(), OpenMeteoTool()], memory=UnconstrainedMemory())
+    agent = ToolCallingAgent(
+        llm=llm,
+        tools=[DuckDuckGoSearchTool(), OpenMeteoTool()],
+        memory=UnconstrainedMemory(),
+        # specify the agent's name and other metadata
+        meta=AgentMeta(name="my_agent", description="A simple agent", tools=[]),
+    )
 
-    AcpServer().register([agent]).serve()
+    # Register the agent with the ACP server and run the HTTP server
+    AcpServer().register([agent]).serve(config=AcpServerConfig(port=8001))
 
 
 if __name__ == "__main__":
