fa7ab67681ad3658db2d3677ed1e57b9bef6f30c
Lukáš Janeček <xjacka@gmail.com>
2025-07-11 17:14:14 +0200

    fix(utils): fix Json Schema additional properties
    Signed-off-by: Lukáš Janeček <xjacka@gmail.com>

--- python/tests/utils/test_json_schema.py
+++ python/tests/utils/test_json_schema.py
@@ -184,6 +184,7 @@ def test_schema_with_additional_properties(schema_with_additional_properties: di
         model.model_validate({"config": {"test": "test"}})
 
     assert model.model_validate({"input": {"query": "test"}})
+    assert model.model_validate({"input": {"query": "test"}}).model_dump()["input"] == {"query": "test"}
 
     assert model.model_fields["input"].annotation.model_fields == {}  # type: ignore
     assert get_args(model.model_fields["config"].annotation)[0].model_fields["max_retries"].annotation is int
