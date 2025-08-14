f21f07d9a1950f1d2d30fe2f8c892b88463c87e9
Lukáš Janeček <xjacka@gmail.com>
2025-08-14 16:00:33 +0200

    feat(serve)!: switch BeeAIPlatform agent and server to beeai-sdk and a2a protocol
    Signed-off-by: Lukáš Janeček <xjacka@gmail.com>

--- python/beeai_framework/adapters/beeai_platform/agents/types.py
+++ python/beeai_framework/adapters/beeai_platform/agents/types.py
@@ -1,7 +1,8 @@
 # Copyright 2025 © BeeAI a Series of LF Projects, LLC
 # SPDX-License-Identifier: Apache-2.0
 
-from acp_sdk.models.models import Event
+from typing import Any
+
 from pydantic import BaseModel, InstanceOf
 
 from beeai_framework.backend.message import AnyMessage
@@ -9,4 +10,4 @@ from beeai_framework.backend.message import AnyMessage
 
 class BeeAIPlatformAgentRunOutput(BaseModel):
     result: InstanceOf[AnyMessage]
-    event: Event
+    event: Any
--- python/examples/serve/beeai_platform.py
+++ python/examples/serve/beeai_platform.py
@@ -1,30 +1,34 @@
 from beeai_framework.adapters.beeai_platform.serve.server import BeeAIPlatformServer
-from beeai_framework.agents.experimental import RequirementAgent
+from beeai_framework.agents.tool_calling import ToolCallingAgent
 from beeai_framework.backend import ChatModel
 from beeai_framework.memory import UnconstrainedMemory
-from beeai_framework.middleware.trajectory import GlobalTrajectoryMiddleware
 from beeai_framework.tools.search.duckduckgo import DuckDuckGoSearchTool
 from beeai_framework.tools.weather import OpenMeteoTool
+
+try:
+    from beeai_sdk.a2a.extensions.ui.agent_detail import AgentDetail
+except ModuleNotFoundError as e:
+    raise ModuleNotFoundError(
+        "Optional module [beeai-platform] not found.\nRun 'pip install \"beeai-framework[beeai-platform]\"' to install."
+    ) from e
 
 
 def main() -> None:
     llm = ChatModel.from_name("ollama:granite3.3:8b")
-    agent = RequirementAgent(
+    agent = ToolCallingAgent(
         llm=llm,
         tools=[DuckDuckGoSearchTool(), OpenMeteoTool()],
         memory=UnconstrainedMemory(),
-        middlewares=[GlobalTrajectoryMiddleware()],
+        # middlewares=[GlobalTrajectoryMiddleware()],
     )
 
     # Runs HTTP server that registers to BeeAI platform
-    server = BeeAIPlatformServer()
+    server = BeeAIPlatformServer(config={"configure_telemetry": False})
     server.register(
         agent,
-        name="granite_chat_agent",
+        name="Granite chat agent",
         description="Simple chat agent",  # (optional)
-        ui={"type": "chat"},  # default is chat (optional)
-        tags=["example"],  # (optional)
-        recommended_models=["granite3.3:8b"],  # (optional)
+        detail=AgentDetail(ui_type="chat"),  # default is chat (optional)
     )
     server.serve()
 
