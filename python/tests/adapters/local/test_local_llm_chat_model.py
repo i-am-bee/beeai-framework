48d4d06b31444a55f4c2ab73a6a6063d19bc8b0e
Jungkoo Kang <jungkoo.kang@gmail.com>
2025-09-23 07:04:37 -0400

    feat(adapters): refactor tests for transformers adapter (#1012)
    Signed-off-by: Jungkoo Kang <jungkoo.kang@gmail.com>

--- python/tests/adapters/local/test_local_llm_chat_model.py
+++ python/tests/adapters/local/test_local_llm_chat_model.py
@@ -1,25 +1,17 @@
 # Copyright 2025 © BeeAI a Series of LF Projects, LLC
 # SPDX-License-Identifier: Apache-2.0
-import json
 import os
-import re
 
 import pytest
 from pydantic import BaseModel
 
 from beeai_framework.backend import (
-    AnyMessage,
     AssistantMessage,
     ChatModelParameters,
     CustomMessage,
-    MessageToolResultContent,
-    SystemMessage,
-    ToolMessage,
     UserMessage,
 )
 from beeai_framework.backend.chat import ChatModel
-from beeai_framework.tools import AnyTool, ToolOutput
-from beeai_framework.tools.weather.openmeteo import OpenMeteoTool
 
 
 @pytest.mark.skipif(
@@ -44,18 +36,6 @@ class TestTransformersChatModel:
         assert len(response.messages[0].content) > 0
         assert all(isinstance(message, AssistantMessage) for message in response.messages)
         assert "10" in response.messages[0].text
-
-    @pytest.mark.e2e
-    @pytest.mark.asyncio
-    async def test_local_llm_chat_model_create_tool_message(self) -> None:
-        tools: list[AnyTool] = [OpenMeteoTool()]
-        tool_messages: list[AnyMessage] = [
-            SystemMessage("You are a helpful assistant. Use tools to provide a correct answer."),
-            UserMessage("What's the fastest marathon time?"),
-        ]
-        answer = await self.get_answer_from_chat_model_for_tool_calls(tools=tools, tool_messages=tool_messages)
-
-        assert len(answer) > 0
 
     @pytest.mark.e2e
     @pytest.mark.asyncio
@@ -85,51 +65,3 @@ class TestTransformersChatModel:
         )
 
         assert isinstance(response.object, dict)
-
-    async def get_answer_from_chat_model_for_tool_calls(
-        self, tools: list[AnyTool], tool_messages: list[AnyMessage]
-    ) -> str:
-        max_iteration = 10
-        answer = ""
-        iteration = 0
-        while iteration < max_iteration:
-            response = await self.chat_model.create(
-                messages=tool_messages,
-                tools=tools,
-            )
-
-            tool_calls = response.get_tool_calls()
-            tool_messages.extend(response.messages)
-
-            tool_results: list[ToolMessage] = []
-
-            for tool_call in tool_calls:
-                print(f"-> running '{tool_call.tool_name}' tool with {tool_call.args}")
-                tool: AnyTool = next(tool for tool in tools if tool.name == tool_call.tool_name)
-                assert tool is not None
-                res: ToolOutput = await tool.run(json.loads(tool_call.args))
-                result = res.get_text_content()
-                print(
-                    f"<- got response from '{tool_call.tool_name}'",
-                    re.sub(r"\s+", " ", result)[:256] + " (truncated)",
-                )
-                tool_results.append(
-                    ToolMessage(
-                        MessageToolResultContent(
-                            result=result,
-                            tool_name=tool_call.tool_name,
-                            tool_call_id=tool_call.id,
-                        )
-                    )
-                )
-
-            tool_messages.extend(tool_results)
-
-            answer = response.get_text_content()
-
-            if answer:
-                break
-
-            iteration += 1
-
-        return answer
