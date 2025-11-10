8f05e1e18f10417104161e56b879b0f4758b58b6
Lukáš Janeček <xjacka@gmail.com>
2025-11-10 12:52:14 +0100

    feat(tools): add method to create OpenAPITool from URL
    Signed-off-by: Lukáš Janeček <xjacka@gmail.com>

--- python/beeai_framework/tools/openapi.py
+++ python/beeai_framework/tools/openapi.py
@@ -1,6 +1,7 @@
 # Copyright 2025 © BeeAI a Series of LF Projects, LLC
 # SPDX-License-Identifier: Apache-2.0
 
+from collections.abc import Iterable
 from typing import Any, Union
 from urllib.parse import parse_qs, urlencode, urljoin, urlparse, urlunparse
 
@@ -40,14 +41,21 @@ class OpenAPITool(Tool[BaseModel, ToolRunOptions, OpenAPIToolOutput]):
         description: str | None = None,
         url: str | None = None,
         headers: dict[str, str] | None = None,
+        *,
+        path: str | None = None,
+        method: str | None = None,
     ) -> None:
         super().__init__()
         self.open_api_schema = open_api_schema
         self.headers = headers or {}
+        self._path = path
+        self._method = method
 
-        self.url = url or next(
+        server_urls = [
             server.get("url") for server in self.open_api_schema.get("servers", []) if server.get("url") is not None
-        )
+        ]
+        self.url = url or (server_urls[0] if server_urls else None)
+
         if self.url is None:
             raise ToolError("OpenAPI schema hasn't any server with url specified. Pass it manually.")
 
@@ -102,26 +110,42 @@ class OpenAPITool(Tool[BaseModel, ToolRunOptions, OpenAPIToolOutput]):
                 return input
 
             input = input.copy()
-            input["components"] = self.open_api_schema["components"].copy()
-            return inline_schema_refs(input, force=True)
+            input["components"] = self.open_api_schema.get("components", {}).copy()
+            new_schema = inline_schema_refs(input, force=True)
+            new_schema.pop("components", None)
+            return new_schema
+
+        skip_default_parameters = False
+        items = list(self.open_api_schema.get("paths", {}).values())
+        if len(items) == 1 and len(items[0].values()) == 1:
+            skip_default_parameters = True
 
         for path, path_spec in self.open_api_schema.get("paths", {}).items():
             for method, method_spec in path_spec.items():
-                properties = {
-                    "path": {
-                        "const": path,
-                        "description": (
-                            "Do not replace variables in path, instead of, put them to the parameters object."
-                        ),
-                    },
-                    "method": {
-                        "const": method,
-                        "description": method_spec.get("summary", method_spec.get("description")),
-                    },
-                }
+                properties = (
+                    {}
+                    if skip_default_parameters
+                    else {
+                        "path": {
+                            "const": path,
+                            "description": (
+                                "Do not replace variables in path, instead of, put them to the parameters object."
+                            ),
+                        },
+                        "method": {
+                            "const": method,
+                            "description": method_spec.get("summary", method_spec.get("description")),
+                        },
+                    }
+                )
 
-                if method_spec.get("requestBody", {}).get("content", {}).get("application/json", {}).get("schema"):
-                    request_body = method_spec["requestBody"]["content"]["application/json"]["schema"]
+                if request_body := (
+                    method_spec.get("requestBody", {}).get("content", {}).get("application/json", {}).get("schema")
+                    or method_spec.get("requestBody", {})
+                    .get("content", {})
+                    .get("application/x-www-form-urlencoded", {})
+                    .get("schema")
+                ):
                     if request_body.get("$ref"):
                         request_body = get_referenced_object(self.open_api_schema, request_body["$ref"])
                     properties["body"] = resolve_schema_refs(request_body)
@@ -142,15 +166,21 @@ class OpenAPITool(Tool[BaseModel, ToolRunOptions, OpenAPIToolOutput]):
                             if ref_obj and "name" in ref_obj and "schema" in ref_obj:
                                 parameters["properties"][ref_obj["name"]] = {  # type: ignore
                                     **ref_obj["schema"],
-                                    "description": ref_obj["name"],
+                                    "description": ref_obj.get("description", None) or ref_obj["name"],
                                 }
 
                     properties["parameters"] = resolve_schema_refs(parameters)
 
+                required = []
+                if not skip_default_parameters:
+                    required.extend(["path", "method"])
+                if properties.get("body", False):
+                    required.append("body")
+
                 schemas.append(
                     {
                         "type": "object",
-                        "required": ["path", "method"],
+                        "required": required,
                         "additionalProperties": False,
                         "properties": properties,
                     }
@@ -158,7 +188,10 @@ class OpenAPITool(Tool[BaseModel, ToolRunOptions, OpenAPIToolOutput]):
 
         schema_models = [
             JSONSchemaModel.create(
-                f"OpenAPIToolInput{to_safe_word(schema['properties']['method']['const'])}{to_safe_word(schema['properties']['path']['const'])}",
+                f"OpenAPIToolInput{to_safe_word(self._name)}"
+                if skip_default_parameters
+                else f"OpenAPIToolInput{to_safe_word(schema['properties']['method']['const'])}"
+                f"{to_safe_word(schema['properties']['path']['const'])}",
                 schema,
             )
             for schema in schemas
@@ -176,7 +209,7 @@ class OpenAPITool(Tool[BaseModel, ToolRunOptions, OpenAPIToolOutput]):
         self, tool_input: BaseModel, options: ToolRunOptions | None, context: RunContext
     ) -> OpenAPIToolOutput:
         input_dict = tool_input.model_dump()
-        parsed_url = urlparse(urljoin(self.url, input_dict.get("path", "")))
+        parsed_url = urlparse(urljoin(self.url, input_dict.get("path", self._path)))
         search_params = parse_qs(parsed_url.query)
         search_params.update(input_dict.get("parameters", {}))
         new_params = urlencode(search_params, doseq=True)
@@ -186,7 +219,7 @@ class OpenAPITool(Tool[BaseModel, ToolRunOptions, OpenAPIToolOutput]):
         try:
             async with httpx.AsyncClient() as client:
                 response = await client.request(
-                    method=input_dict["method"],
+                    method=input_dict.get("method", self._method) or "",
                     url=str(url),
                     headers={"Accept": "application/json"}.update(self.headers),
                     data=input_dict.get("body"),
@@ -196,3 +229,28 @@ class OpenAPITool(Tool[BaseModel, ToolRunOptions, OpenAPIToolOutput]):
                 return output
         except httpx.HTTPError as err:
             raise ToolError(f"Request to {url} has failed.", cause=err)
+
+    @classmethod
+    async def from_url(cls, open_api_url: str, *, api_url: str | None = None) -> list["OpenAPITool"]:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(open_api_url)
+            return cls.from_schema(response.json(), api_url=api_url)
+
+    @classmethod
+    def from_schema(cls, open_api_schema: dict[str, Any], api_url: str | None = None) -> list["OpenAPITool"]:
+        def get_tool() -> Iterable[OpenAPITool]:
+            for path, path_spec in open_api_schema.get("paths", {}).items():
+                for method, method_spec in path_spec.items():
+                    if isinstance(method_spec, dict):
+                        new_schema = open_api_schema.copy()
+                        new_schema["paths"] = {path: {method: method_spec}}
+                        yield OpenAPITool(
+                            open_api_schema=new_schema,
+                            name=f"{method.lower()} {path}",
+                            description=method_spec.get("summary", method_spec.get("description")),
+                            url=api_url,
+                            path=path,
+                            method=method,
+                        )
+
+        return list(get_tool())
