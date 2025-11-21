ec35ae95eb86216ed0441ad56f8f9f272d30d9ee
Tomáš Dvořák <toomas2d@gmail.com>
2025-11-21 18:35:58 +0100

    feat(agents): rewrite RequirementAgent
    Signed-off-by: Tomáš Dvořák <toomas2d@gmail.com>

---
+++ python/beeai_framework/agents/requirement/_runner.py
@@ -0,0 +1,278 @@
+# Copyright 2025 © BeeAI a Series of LF Projects, LLC
+# SPDX-License-Identifier: Apache-2.0
+
+import uuid
+from typing import Any
+
+from beeai_framework.agents import AgentError, AgentExecutionConfig
+from beeai_framework.agents.requirement.events import (
+    RequirementAgentFinalAnswerEvent,
+    RequirementAgentStartEvent,
+    RequirementAgentSuccessEvent,
+)
+from beeai_framework.agents.requirement.prompts import RequirementAgentToolErrorPromptInput
+from beeai_framework.agents.requirement.requirements import Requirement, Rule
+from beeai_framework.agents.requirement.types import (
+    RequirementAgentRequest,
+    RequirementAgentRunState,
+    RequirementAgentRunStateStep,
+    RequirementAgentTemplates,
+)
+from beeai_framework.agents.requirement.utils._llm import RequirementsReasoner, _create_system_message
+from beeai_framework.agents.requirement.utils._tool import FinalAnswerTool, _run_tools
+from beeai_framework.agents.tool_calling.utils import ToolCallChecker
+from beeai_framework.backend import (
+    AnyMessage,
+    AssistantMessage,
+    ChatModel,
+    ChatModelOutput,
+    MessageToolCallContent,
+    MessageToolResultContent,
+    ToolMessage,
+)
+from beeai_framework.backend.utils import parse_broken_json
+from beeai_framework.context import RunContext
+from beeai_framework.memory import UnconstrainedMemory
+from beeai_framework.middleware.stream_tool_call import StreamToolCallMiddleware
+from beeai_framework.tools import AnyTool
+from beeai_framework.utils.counter import RetryCounter
+from beeai_framework.utils.strings import find_first_pair, generate_random_string, to_json
+
+
+class RequirementAgentRunner:
+    def __init__(
+        self,
+        *,
+        config: AgentExecutionConfig,
+        tool_call_cycle_checker: ToolCallChecker,
+        force_final_answer_as_tool: bool,
+        expected_output: Any,
+        run_context: RunContext,
+        requirements: list[Requirement[RequirementAgentRunState]],
+        tools: list[AnyTool],
+        templates: RequirementAgentTemplates,
+        llm: ChatModel,
+    ) -> None:
+        self._ctx = run_context
+        self._llm = llm
+        self._templates = templates
+        self._force_final_answer_as_tool = force_final_answer_as_tool
+        self._state = RequirementAgentRunState(
+            answer=None, result=None, memory=UnconstrainedMemory(), steps=[], iteration=0
+        )
+        self._requirements = requirements
+        self._reasoner = RequirementsReasoner(
+            tools=tools,
+            final_answer=FinalAnswerTool(expected_output, state=self._state),
+            context=run_context,
+        )
+        self._run_config = config
+        self._tool_call_cycle_checker = tool_call_cycle_checker
+        self._tool_call_retry_counter = RetryCounter(
+            error_type=AgentError,
+            max_retries=0 if config.total_max_retries is None else config.total_max_retries,
+        )
+
+    def _increment_iteration(self) -> None:
+        self._state.iteration += 1
+
+        if self._run_config.max_iterations and self._state.iteration > self._run_config.max_iterations:
+            raise AgentError(f"Agent was not able to resolve the task in {self._state.iteration} iterations.")
+
+    def __create_final_answer_stream(self) -> StreamToolCallMiddleware:
+        stream_middleware = StreamToolCallMiddleware(
+            self._reasoner.final_answer,
+            "response",  # from the default schema
+            match_nested=False,
+            force_streaming=False,
+        )
+        stream_middleware.emitter.on(
+            "update",
+            lambda data, meta: self._ctx.emitter.emit(
+                "final_answer",
+                RequirementAgentFinalAnswerEvent(
+                    state=self._state, output=data.output, delta=data.delta, output_structured=None
+                ),
+            ),
+        )
+        return stream_middleware
+
+    async def _send_request(
+        self,
+        request: RequirementAgentRequest,
+    ) -> ChatModelOutput:
+        stream_middleware = self.__create_final_answer_stream()
+        response = await self._llm.run(
+            [
+                _create_system_message(
+                    template=self._templates.system,
+                    request=request,
+                ),
+                *self._state.memory.messages,
+            ],
+            max_retries=self._run_config.max_retries_per_step,
+            tools=request.allowed_tools,
+            tool_choice=request.tool_choice,
+            stream_partial_tool_calls=True,
+        ).middleware(stream_middleware)
+        stream_middleware.unbind()
+        return response
+
+    async def _process_response(self, response: ChatModelOutput, request: RequirementAgentRequest) -> ChatModelOutput:
+        if response.get_tool_calls():  # all good
+            return response
+
+        text = response.get_text_content()
+        if text and request.can_stop:
+            fixed_message = await self.__create_final_answer_tool_call(text)
+            if fixed_message:
+                cloned = ChatModelOutput.from_chunks([response])
+                cloned.output_structured = None
+                cloned.output = [fixed_message]
+                return cloned
+
+        raise ValueError("No final answer tool found.")
+
+    async def __create_final_answer_tool_call(self, full_text: str) -> AssistantMessage | None:
+        json_object_pair = find_first_pair(full_text, ("{", "}"))
+        final_answer_input = parse_broken_json(json_object_pair.outer) if json_object_pair else None
+        if not final_answer_input and not self._reasoner.final_answer.custom_schema:
+            final_answer_input = {"response": full_text}
+
+        if not final_answer_input:
+            return None
+        else:
+            manual_assistant_tool_call_message = MessageToolCallContent(
+                type="tool-call",
+                id=f"call_{generate_random_string(8).lower()}",
+                tool_name=self._reasoner.final_answer.name,
+                args=to_json(final_answer_input, sort_keys=False),
+            )
+            # tool_call_messages.append(manual_assistant_tool_call_message)
+            # await state.memory.add(AssistantMessage(manual_assistant_tool_call_message))
+            return AssistantMessage(manual_assistant_tool_call_message)
+
+    async def _create_request(self, *, extra_rules: list[Rule] | None = None) -> RequirementAgentRequest:
+        return await self._reasoner.create_request(
+            self._state,
+            force_tool_call=self._force_final_answer_as_tool,
+            extra_rules=extra_rules,
+        )
+
+    async def _invoke_tool_calls(
+        self, tools: list[AnyTool], tool_calls: list[MessageToolCallContent]
+    ) -> list[ToolMessage]:
+        tool_results: list[ToolMessage] = []
+        for tool_call in await _run_tools(
+            tools=tools,
+            messages=tool_calls,
+            context={"state": self._state.model_dump()},
+        ):
+            self._state.steps.append(
+                RequirementAgentRunStateStep(
+                    id=str(uuid.uuid4()),
+                    iteration=self._state.iteration,
+                    input=tool_call.input,
+                    output=tool_call.output,
+                    tool=tool_call.tool,
+                    error=tool_call.error,
+                )
+            )
+
+            if tool_call.error is not None:
+                result = self._templates.tool_error.render(
+                    RequirementAgentToolErrorPromptInput(reason=tool_call.error.explain())
+                )
+            else:
+                result = (
+                    tool_call.output.get_text_content()
+                    if not tool_call.output.is_empty()
+                    else self._templates.tool_no_result.render(tool_call=tool_call)
+                )
+
+            tool_results.append(
+                ToolMessage(
+                    MessageToolResultContent(
+                        tool_name=tool_call.tool.name if tool_call.tool else tool_call.msg.tool_name,
+                        tool_call_id=tool_call.msg.id,
+                        result=result,
+                    )
+                )
+            )
+            if tool_call.error is not None:
+                self._tool_call_retry_counter.use(tool_call.error)
+
+        return tool_results
+
+    async def add_messages(self, messages: list[AnyMessage]) -> None:
+        await self._state.memory.add_many(messages)
+
+    async def run(self) -> RequirementAgentRunState:
+        if self._state.answer is not None:
+            return self._state
+
+        # Init
+        await self._reasoner.update(self._requirements)
+
+        while self._state.answer is None:
+            self._state.iteration += 1
+
+            if self._run_config.max_iterations and self._state.iteration > self._run_config.max_iterations:
+                raise AgentError(f"Agent was not able to resolve the task in {self._state.iteration} iterations.")
+
+            request = await self._create_request()
+            await self._ctx.emitter.emit(
+                "start",
+                RequirementAgentStartEvent(state=self._state, request=request),
+            )
+            response = await self.__run(request)
+            await self._ctx.emitter.emit(
+                "success",
+                RequirementAgentSuccessEvent(state=self._state, response=response),
+            )
+        return self._state
+
+    async def __run(self, request: RequirementAgentRequest) -> ChatModelOutput:
+        response_raw = await self._send_request(request)
+        response = await self._process_response(response_raw, request)  # TODO: handle errors
+
+        if response.is_empty():
+            await self._state.memory.add(AssistantMessage("\n", {"tempMessage": True}))
+            return await self.__run(request)
+
+        if not response.get_tool_calls():  # all good
+            text = response.get_text_content()
+            if text and request.can_stop:
+                fixed_tool_call = await self.__create_final_answer_tool_call(text)
+                if fixed_tool_call:
+                    response.output_structured = None
+                    response.output = [fixed_tool_call]
+                else:
+                    self._force_final_answer_as_tool = True
+                    await self._reasoner.update(requirements=[])
+                    updated_request = await self._create_request(
+                        extra_rules=[Rule(target=self._reasoner.final_answer.name, allowed=True, hidden=False)],
+                    )
+                    return await self.__run(updated_request)
+            else:
+                raise ValueError("No final answer tool found.")
+
+        # Check Tool Calls
+        tool_calls = response.get_tool_calls()
+        for tool_call_msg in tool_calls:
+            self._tool_call_cycle_checker.register(tool_call_msg)
+            if self._tool_call_cycle_checker.cycle_found:
+                self._tool_call_cycle_checker.reset()
+                updated_request = await self._create_request(
+                    extra_rules=[Rule(target=tool_call_msg.tool_name, allowed=False, hidden=False, forced=True)],
+                )
+                return await self.__run(updated_request)
+
+        tool_results = await self._invoke_tool_calls(request.allowed_tools, tool_calls)
+
+        await self._state.memory.add_many([*response.output, *tool_results])
+        await self._state.memory.delete_many(
+            [msg for msg in self._state.memory.messages if msg.meta.get("tempMessage", False)]
+        )
+
+        return response
